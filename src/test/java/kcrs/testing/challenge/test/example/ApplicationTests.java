7fd54716549a9b950029b8a205f4223ebb4aa2aa
ferneyunimag <ferneyunimag@gmail.com>
2022-07-26 11:17:46 -0500

    inicio respositorio

---
+++ src/test/java/kcrs/testing/challenge/test/example/ApplicationTests.java
@@ -0,0 +1,13 @@
+package kcrs.testing.challenge.test.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
